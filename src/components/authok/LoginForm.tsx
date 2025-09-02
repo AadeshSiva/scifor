02bf926f38558bfc569fab28c65cf32fee4bd2a7
Shudhanshu Kumar <shudhanshukumar69@gmail.com>
2025-09-02 11:51:05 -0700

    Update LoginForm.tsx

--- src/components/authok/LoginForm.tsx
+++ src/components/authok/LoginForm.tsx
@@ -34,7 +34,7 @@ const ForgotPasswordModal: React.FC<{
   const [errorMessage, setErrorMessage] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
 
-  const API_BASE_URL = "http://31.97.117.28:8001";
+  const API_BASE_URL = "https://internship-pro.onrender.com";
 
   const resetForm = () => {
     setStep(1);
