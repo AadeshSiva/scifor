01a6aba1d706c09a821d9e7ff27f7d357208583a
imshus <shudhanshukumar69@gmail.com>
2025-09-08 19:17:12 +0530

    feat: add google meet

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,6 @@ const App = () => (
           <div className="fixed top-0 left-0 w-full z-50">
             <NavBar />
           </div>
-
           <Routes>
             <Route
               path="/otpverify"
