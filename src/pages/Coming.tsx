b2a8a624dc41cdea46a5f90c76e947396b0a440b
imshus <shudhanshukumar69@gmail.com>
2025-09-11 17:53:32 +0530

    fix: improve coming.tsx

--- src/pages/Coming.tsx
+++ src/pages/Coming.tsx
@@ -4,7 +4,7 @@ const Coming: React.FC=()=> {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-600 text-white space-y-4">
       <h1 className="text-4xl font-bold">Coming Soon</h1>
-      <button><a href="/dashboard/" className="text-2xl font-bold text-black border p-4 hover:bg-blue-400">Back to Dashboard</a></button>
+      <button><a href="/dashboard" className="text-2xl font-bold text-black border p-4 hover:bg-blue-400">Back to Dashboard</a></button>
     </div>
   );
 }
